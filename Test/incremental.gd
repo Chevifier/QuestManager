034d8b3ebe06336a84383d9279cc31cdc355724a
Chevon <chevgc@gmail.com>
2024-08-07 17:24:36 -0400

    Updated Quest Progression System

--- Test/incremental.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-signal item_collected(n)
-@export var item_name = "Apple"
-func _ready() -> void:
-	#connect signal to quest manager item
-	item_collected.connect(QuestManager.set_item_collected)
-func _on_add_item_pressed() -> void:
-	item_collected.emit(item_name)
